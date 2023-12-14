362fb2289b24b6be77e09bed3a1c9863781c81d5
Juanrojas0399 <110487216+Juanrojas0399@users.noreply.github.com>
2023-12-14 16:15:42 -0500

    Update wsgi.py

--- sovereing-king-motorsports/sovereing-king-motorsports/wsgi.py
+++ sovereing-king-motorsports/sovereing-king-motorsports/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SOVERING_KING_MOTORSPORTS.settings')
+
+#application = get_wsgi_application()
+
+
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SOVERING_KING_MOTORSPORTS.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sovereing-king-motorsports.settings")
 application = get_wsgi_application()
+app = application
