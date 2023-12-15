8cc7f4031d44b2704f7cfc5ed34ad6102cd90c52
Juanrojas0399 <110487216+Juanrojas0399@users.noreply.github.com>
2023-12-15 01:13:27 -0500

    Update settings.py

--- sovereing-king-motorsports/sovereing-king-motorsports/settings.py
+++ sovereing-king-motorsports/sovereing-king-motorsports/settings.py
@@ -66,6 +66,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
 #    'http://localhost',
 #    'http://127.0.0.1',
