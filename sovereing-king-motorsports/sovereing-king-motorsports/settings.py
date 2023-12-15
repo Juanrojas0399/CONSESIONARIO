ad92ccd27b398723ee4fb211a10335aac7571434
Juanrojas0399 <110487216+Juanrojas0399@users.noreply.github.com>
2023-12-14 23:05:26 -0500

    Update settings.py

--- sovereing-king-motorsports/sovereing-king-motorsports/settings.py
+++ sovereing-king-motorsports/sovereing-king-motorsports/settings.py
@@ -36,7 +36,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 ALLOWED_HOSTS = [
-    'consesionario-5vje6vdh5-juan-rojas-projects.vercel.app',
+    'consesionario-juan-rojas-projects.vercel.app',
 ]
 
 
